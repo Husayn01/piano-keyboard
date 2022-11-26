daa3132b32e69f43adac0bd9086ceca2f8cc5dff
Hussayn <94265187+Husayn01@users.noreply.github.com>
2022-11-26 01:10:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+//Container C
+document.querySelector(".c1").addEventListener("click", handleClickC1 );
+    function handleClickC1() {
+        var audio = new Audio('audio/448547__tedagame__c2.ogg');
+        audio.play();
+}
+
+document.querySelector(".c2").addEventListener("click", handleClickC2 );
+    function handleClickC2() {
+        var audio = new Audio('audio/448541__tedagame__c-2.ogg');
+        audio.play();
+}
+
+
+//Container D
+document.querySelector(".d1").addEventListener("click", handleClickD1 );
+    function handleClickD1() {
+        var audio = new Audio('audio/448607__tedagame__d2.ogg');
+        audio.play();
+}
+
+document.querySelector(".d2").addEventListener("click", handleClickD2 );
+    function handleClickD2() {
+        var audio = new Audio('audio/448600__tedagame__d-2.ogg');
+        audio.play();
+}
+
+//Container E
+document.querySelector(".e2").addEventListener("click", handleClickE2 );
+    function handleClickE2() {
+        var audio = new Audio('audio/448600__tedagame__d-2.ogg');
+        audio.play();
+}
+
+//Container F
+document.querySelector(".f1").addEventListener("click", handleClickF1 );
+    function handleClickF1() {
+        var audio = new Audio('audio/448588__tedagame__f2.ogg');
+        audio.play();
+}
+
+document.querySelector(".f2").addEventListener("click", handleClickF2 );
+    function handleClickF2() {
+        var audio = new Audio('audio/448587__tedagame__f-2.ogg');
+        audio.play();
+}
+
+// Container G
+document.querySelector(".g1").addEventListener("click", handleClickG1 );
+    function handleClickG1() {
+        var audio = new Audio('audio/448558__tedagame__g2.ogg');
+        audio.play();
+}
+
+document.querySelector(".g2").addEventListener("click", handleClickG2 );
+    function handleClickG2() {
+        var audio = new Audio('audio/448590__tedagame__g-2.ogg');
+        audio.play();
+}
+
+// Container A
+document.querySelector(".a1").addEventListener("click", handleClickA1 );
+    function handleClickA1() {
+        var audio = new Audio('audio/448563__tedagame__a2.ogg');
+        audio.play();
+}
+
+document.querySelector(".a2").addEventListener("click", handleClickA2 );
+    function handleClickA2() {
+        var audio = new Audio('audio/448571__tedagame__a-2.ogg');
+        audio.play();
+}
+
+//Container B
+document.querySelector(".b2").addEventListener("click", handleClickB2 );
+    function handleClickB2() {
+        var audio = new Audio('audio/448569__tedagame__b2.ogg');
+        audio.play();
+}
